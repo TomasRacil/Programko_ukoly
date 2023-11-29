73c27eaaa2bd9ece442d9040036bc321d6a7d11d
Denis Štěpán <116164832+Bublos@users.noreply.github.com>
2023-11-29 15:40:06 +0100

    Delete Python_Flappy directory

--- Python_Flappy/functions/jump_count.py
+++
@@ -1,7 +0,0 @@
-import pygame
-def jump_count(space_count:int)->int:
-    space_count = 0
-    key = pygame.key.get_pressed()
-    if key[pygame.K_SPACE]:
-        space_count = 1
-    return space_count
--- Python_Flappy/functions/text.py
+++
@@ -1,4 +0,0 @@
-from variables.variables import screen
-def text(text:str, x:int, y:int, font, text_color):
-    image = font.render(text, True, text_color)
-    screen.blit(image,(x,y))
